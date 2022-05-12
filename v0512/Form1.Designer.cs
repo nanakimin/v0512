9314348fc4810327187f132cf170522fbe0e0049
A601-25 <oudontabetai0101@gmail.com>
2022-05-12 15:44:31 +0900

    ボタンを消す

--- v0512/Form1.Designer.cs
+++ v0512/Form1.Designer.cs
@@ -29,25 +29,27 @@ namespace v0512
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.maibutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // maibutton
             // 
-            this.button1.Location = new System.Drawing.Point(254, 68);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(65, 33);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.maibutton.BackColor = System.Drawing.Color.Lime;
+            this.maibutton.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.maibutton.Location = new System.Drawing.Point(254, 68);
+            this.maibutton.Name = "maibutton";
+            this.maibutton.Size = new System.Drawing.Size(114, 40);
+            this.maibutton.TabIndex = 0;
+            this.maibutton.Text = "button1";
+            this.maibutton.UseVisualStyleBackColor = false;
+            this.maibutton.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maibutton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,7 +58,7 @@ namespace v0512
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button maibutton;
     }
 }
 
